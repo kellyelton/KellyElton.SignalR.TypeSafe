c29e82c368f28d4439d4da3c5aed55b4f9a0e37e
Kelly Elton <kelly.elton@skylabsonline.com>
2013-02-23 19:37:54 -0600

    Added Example projects.

---
+++ example/HubServer/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace HubServer
+{
+    using System;
+    using System.Threading;
+
+    using Microsoft.AspNet.SignalR;
+    using Microsoft.Owin.Hosting;
+
+    using Owin;
+
+    class Program
+    {
+        internal static IDisposable Host;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting Hub");
+            Host = WebApplication.Start<Setup>("http://localhost:12345");
+            Console.WriteLine("Hub Started");
+
+            Console.WriteLine("Press the q key to quit.");
+
+            var keepRunning = true;
+            while (keepRunning)
+            {
+                if (Console.KeyAvailable) if (Console.ReadKey().Key == ConsoleKey.Q) keepRunning = false;
+                Thread.Sleep(10);
+            }
+        }
+    }
+    public class Setup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapHubs(new HubConfiguration());
+        }
+    }
+}
